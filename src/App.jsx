29775d17fac57052cf512d3aeb0e9a72f2200da4
Ryo-1157 <75074471+Ryo-1157@users.noreply.github.com>
2022-01-05 19:07:04 +0900

    再レンダリングの方法を学習 memo(コンポーネントのメモ化) useCallback(関数のメモ化) useMemo(変数のメモ化)を覚えたよ

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,44 @@
-import { useState } from "react";
+import { useCallback, useState, useMemo } from "react";
 import "./styles.css";
 import { ChildArea } from "./components/index";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount((prevState) => prevState + 1);
+  console.log("Appがレンダリングされました。");
+  // const [count, setCount] = useState(0);
+  // const countUp = () => {
+  //   setCount((prevState) => prevState + 1);
+  // };
+  // const countDown = () => {
+  //   setCount((prevState) => prevState - 1);
+  // };
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+  /**
+   * useCallbackは関数のmemo化のこと。
+   * memo化はpropsの書き換えが起こらない限り再レンダリングが起こらないようにすることをさす。
+   * しかしアロー関数の場合、毎回新しい関数が作られpropsとして渡される判定になってしまう。
+   * そこでアロー関数をuseCallbackで囲むことで、一番初めのレンダリングでしか読み込まれないようにする処理ができる。
+   */
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  const onChangeText = (event) => {
+    setText(event.target.value);
   };
-  const countDown = () => {
-    setCount((prevState) => prevState - 1);
-  };
+  const onClickOpen = () => setOpen(!open);
   return (
     <div className="App">
-      <input />
+      <input value={text} onChange={onChangeText} />
       <br />
       <br />
-      <button>表示</button>
-      <ChildArea></ChildArea>
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea
+        open={open}
+        // もしもindexのような値を入れたいもののをuseCallbackしたいなら、propsを渡す時に書けばいい。ほぼそんな時は来ないけど。
+        onClickClose={useCallback(() => onClickClose(), [onClickClose])}
+      ></ChildArea>
     </div>
   );
 }
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,11 +1,31 @@
-import React from "react";
-
-const ChildArea = () => {
-  return (
-    <div>
-      <p>子コンポーネント</p>
-    </div>
-  );
+import React, { memo } from "react";
+// コンポーネントをmemoで囲ってあげるとpropsの書き換えが起きる時だけレンダリングされるようになり、親要素と合わせてレンダリングされなくなる。
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
 };
+
+const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log("ChildAreaがレンダリングされました。");
+
+  const data = [...Array(2000).keys()];
+  // data.forEach(() => {
+  //   console.log("...");
+  // });
+  return (
+    <>
+      {open ? (
+        // これは三項演算子を使って、 openがTrueならdivタグをレンダリングし、
+        //　falseならnullを返す（＝何も表示しない）という処理を行っている
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
 
 export default ChildArea;
